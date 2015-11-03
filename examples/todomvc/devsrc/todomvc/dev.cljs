6c487692a7fa391930a98857f57735ac579d9697
Antonin Hildebrand <antonin@hildebrand.cz>
2015-11-04 00:48:53 +0100

    delete examples
    original re-frame should be consulted instead

--- examples/todomvc/devsrc/todomvc/dev.cljs
+++
@@ -1,9 +0,0 @@
-(ns todomvc.dev
-  (:require [todomvc.core :as todomvc]
-            [figwheel.client :as fw]
-            [devtools.core :as devtools]))
-
-(devtools/install!)
-
-(fw/start {:on-jsload     todomvc/main
-           :websocket-url "ws://localhost:3450/figwheel-ws"})
